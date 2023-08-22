68a928633f18bb419555766ad84a63db092011ba
danate1997 <daniel.atehortua1997@gmail.com>
2023-08-22 14:48:53 -0400

    added row function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your code here
+// HTML elements
+const rowButton = document.getElementById("add-row");
+const table = document.getElementById("table");
+
+function makeARow() {
+    const tr=document.createElement("tr");
+    for (let i=0; i < 20; i++) {
+        const td=document.createElement("td");
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+    
+}
+console.log(makeARow())
+// makeARow()
+// makeARow()
+// makeARow()
+// makeARow()
+
+rowButton.addEventListener("click",() => {
+    makeARow();
+}) 
