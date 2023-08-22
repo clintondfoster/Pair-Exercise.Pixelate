30b16b8b62f9fc75dd3f455d4c043f12dd40dc91
clinton foster <clintondavidfoster@gmail.com>
2023-08-22 15:22:26 -0400

    added color functions and select events

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 // HTML elements
 const rowButton = document.getElementById("add-row");
 const table = document.getElementById("table");
+const select = document.getElementById("select");
 
+
+//Add Row Functionality 
 function makeARow() {
     const tr=document.createElement("tr");
     for (let i=0; i < 20; i++) {
@@ -10,14 +13,35 @@ function makeARow() {
         tr.appendChild(td);
     }
     table.appendChild(tr);
-    
 }
-console.log(makeARow())
-// makeARow()
-// makeARow()
-// makeARow()
-// makeARow()
+// console.log(makeARow())
 
 rowButton.addEventListener("click",() => {
     makeARow();
-}) 
+});
+
+
+//Click functionality event listener
+function colorize (e) {
+    const tdCell = e.target;
+    if (tdCell.className.length) {
+        target.className = '';
+    } else {
+        tdCell.className = chosenColor;
+    }
+    console.log('clicked')
+}
+
+table.addEventListener("click", colorize)
+
+//Color change event
+let chosenColor = 'pink';
+
+select.addEventListener("change", (e) => {
+    chosenColor = e.target.value;
+})
+
+
+
+
+
